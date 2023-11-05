f7cf433277fe7d3a5d01b00e249fb899b4788aa7
sabbir1791 <sabbirh8181@gmail.com>
2023-11-05 15:45:13 +0600

    marquee added with banner in the home page

---
+++ src/Components/Pages/Home/Banner/Marquees.jsx
@@ -0,0 +1,28 @@
+import Marquee from "react-fast-marquee";
+import { Link } from "react-router-dom";
+
+const Marquees = () => {
+  return (
+    <div className="flex">
+      <button className="btn bg-[#f3701d] border-[#f3701d] text-white hover:text-black capitalize">
+        Our Book Items
+      </button>
+      <Marquee pauseOnHover={true} speed={100}>
+        <Link className="mr-12" to="/biographies">
+          Biographies
+        </Link>
+        <Link className="mr-12" to="/history">
+          History
+        </Link>
+        <Link className="mr-12" to="/religion">
+          Religion
+        </Link>
+        <Link className="mr-12" to="/business">
+          Business
+        </Link>
+      </Marquee>
+    </div>
+  );
+};
+
+export default Marquees;
