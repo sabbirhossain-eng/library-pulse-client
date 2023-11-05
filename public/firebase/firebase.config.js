a307fb087f11682ad91eda6e3fbf4e76d83376c4
sabbir1791 <sabbirh8181@gmail.com>
2023-11-05 18:55:08 +0600

    dotenv fiile add and gitignore

--- public/firebase/firebase.config.js
+++ public/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB4MZh5kq5DrRXUGluzblZ5bQP1Q67k37M",
-  authDomain: "library-pulse.firebaseapp.com",
-  projectId: "library-pulse",
-  storageBucket: "library-pulse.appspot.com",
-  messagingSenderId: "1098150828322",
-  appId: "1:1098150828322:web:d06b598fda7b0add086d7b"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
